870bebd54c49d9db34f0ef8d765647bb8ec7aa54
KALPIT NAGAR <kalpitnagar312@gmail.com>
2025-11-01 00:15:50 +0530

    .

--- LAB 3+4/32_max_secondmax.cpp
+++ LAB 3+4/32_max_secondmax.cpp
@@ -1,32 +1,30 @@
-// program to find max and second max
 #include <iostream>
 using namespace std;
 
 int main() {
-    int n, a, x = 0, y = 0, z = 0;
-    cout << "Enter the number of numbers required: ";
+    int n;
+    cout << "Enter number of elements: ";
     cin >> n;
-    for (int i = 0; i < n; i++) {
-        cout << "Enter the number: ";
-        cin >> a;
-        if (a > x) {
-            x = x + a;
-            a = x - a;
-            x = x - a;
+
+    int a[n];
+    cout << "Enter elements: ";
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+
+    int max1 = a[0], max2 = -1e9;   // initialize max and second max
+
+    for(int i = 1; i < n; i++) {
+        if(a[i] > max1) {
+            max2 = max1;
+            max1 = a[i];
         }
-        if (a > y && a < x) {
-            y = y + a;
-            a = y - a;
-            y = y - a;
-        }
-        if (a > z && a < x && a < y) {
-            z = z + a;
-            a = z - a;
-            z = z - a;
+        else if(a[i] > max2 && a[i] < max1) {
+            max2 = a[i];
         }
     }
-    cout << "The 1st greatest is " << x << endl;
-    cout << "The 2nd greatest is " << y << endl;
+
+    cout << "\nMaximum: " << max1;
+    cout << "\nSecond Maximum: " << max2;
 
     return 0;
 }
